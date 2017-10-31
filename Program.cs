a59f652a6d72e321acfa5f67733468c125bf5eb0
Koenkk <koenkanters94@gmail.com>
2017-10-31 19:08:15 +0100

    Initial code commit

---
+++ Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLMerge
+{
+    class Program
+    {
+        private static string datasetname = "AfasGetConnector";
+        private static string itemname = "_MI2_MedewerkerJournaalpost";
+
+
+        private static ArrayList initializeParameters(string directory, string filename)
+        {
+            ArrayList files = new ArrayList();
+
+            // Verify that filename has at least 1 /
+            string[] filenameSplit = filename.Split('\\');
+            if (filenameSplit.Count() == 0 || filenameSplit.Last().Length == 0)
+            {
+                exitWithMessage("Invalid output path: " + filename);
+            }
+
+            // Verify that the directory exists
+            string filenameDirectory = filename.Substring(0, filename.LastIndexOf("\\"));
+
+            if(!Directory.Exists(filenameDirectory))
+            {
+                exitWithMessage("Directory: " + filenameDirectory + " does not exist");
+            }
+
+            // Verify that filename direcot
+            if (File.Exists(filename))
+            {
+                exitWithMessage("File does already exist: " + filename);
+            }
+
+            // Check if directory exists
+            if (Directory.Exists(directory))
+            {
+                // Check if any xml files exist in direcotry
+                string[] fileList = Directory.GetFiles(directory);
+
+                foreach (string file in fileList)
+                {
+                    if (file.EndsWith(".xml"))
+                    {
+                        files.Add(file);
+                    }
+                }
+            }
+            else
+            {
+                exitWithMessage("Directory: " + directory + " does not exist");
+            }
+
+            if (files.Count == 0)
+            {
+                exitWithMessage("No XML files found in directory: " + directory);
+            }
+
+            if (filename.Length == 0)
+            {
+                exitWithMessage("No XML files found in directory: " + directory);
+            }
+
+
+            return files;
+        }
+
+        private static void exitWithMessage(string message)
+        {
+            Console.WriteLine("ERROR: " + message);
+            System.Environment.Exit(1);
+        }
+
+        private static void setTableNames(DataSet ds, string tableName)
+        {
+            foreach (DataTable d in ds.Tables)
+            {
+                d.TableName = tableName;
+            }
+        }
+        
+        private static DataSet mergeXMLFiles(ArrayList files)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.DTD;
+
+            // Create the first dataset and XMLreader
+            DataSet ds = new DataSet();
+            ds.DataSetName = datasetname;
+
+            XmlReader xmlreader = XmlReader.Create((string) files[0], settings);
+
+            try
+            {
+                ds.ReadXml(xmlreader);
+
+                setTableNames(ds, itemname);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(files[0] + " contains invalid XML, skipped..");
+            }
+
+           
+
+            for (int i = 1; i < files.Count; i++)
+            {
+                // Read out XML and merge with others
+                string file = (string)files[i];
+                XmlReader xmlreader2 = XmlReader.Create(file, settings);
+                DataSet ds2 = new DataSet();
+                ds2.DataSetName = datasetname;
+
+                try
+                {
+                    ds2.ReadXml(xmlreader2);
+                    setTableNames(ds2, itemname);
+                    ds.Merge(ds2);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(file + " contains invalid XML, skipped..");
+                }
+                
+               
+            }
+
+            return ds;
+        }
+
+        private static string writeXML(DataSet ds, string directory, string output)
+        { 
+            ds.WriteXml(output, XmlWriteMode.WriteSchema);
+
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2)
+                {
+                    exitWithMessage("Incorrect amount of parameters, use <directory> <outputfile>");
+                }
+
+                string directory = args[0];
+                string outputFile = args[1];
+
+                if (outputFile.EndsWith("\\"))
+                {
+                    outputFile = outputFile + "output";
+                }
+
+                if (!outputFile.EndsWith(".xml"))
+                {
+                    outputFile = outputFile + ".xml";
+                }
+
+                // Intialize and check parameters
+                ArrayList xmlFiles = initializeParameters(directory, outputFile);
+
+                if (xmlFiles.Count == 1)
+                {
+                    exitWithMessage("Only 1 XML file found, no need to merge");
+                }
+
+                Console.WriteLine("Will merge the following XML files:");
+                foreach (string s in xmlFiles)
+                {
+                    Console.WriteLine(s);
+                }
+
+                // Merge XML files 
+                DataSet dataSet = mergeXMLFiles(xmlFiles);
+
+                // Write merged XML file
+                string output = writeXML(dataSet, directory, outputFile);
+                Console.WriteLine("Merged XML to: " + output);
+            } catch (Exception ex)
+            {
+                exitWithMessage("An error occured");
+            }
+        }
+    }
+}
